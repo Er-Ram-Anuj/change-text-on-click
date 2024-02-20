495382a86dee08df93798f77a566d18992b79ce7
Ram Anuj <121351615+Er-Ram-Anuj@users.noreply.github.com>
2024-02-20 17:33:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function changeText(){
+    let text=document.getElementById("heading");
+    if(text.innerText==="The most affordable lerning platform is"){
+        text.innerText="PW Skill";
+    }
+else{
+    text.innerText=("The most affordable lerning platform is");
+}
+}
